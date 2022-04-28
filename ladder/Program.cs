502a4e751678b48fe9d297e33123ec07147e207c
sgurnik00@gmail.com <sgurnik00@gmail.com>
2022-04-28 16:03:05 +0300

    Add project files.

---
+++ ladder/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+for (int i = 0; i < m; i++)
+{
+    n+=2;
+    for (int j = 0; j < n; j++) Console.Write(" ");
+ 
+    Console.WriteLine("***");
+    for (int j = 0; j < n; j++) Console.Write(" ");
+    Console.WriteLine("  *");
+
+
+}
+
+   
+
